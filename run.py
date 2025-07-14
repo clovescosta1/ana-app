0e9088e621d7e702a1157e6ad365afd406cd7832
clovescosta1 <clovescosta2009@hotmail.com>
2025-07-14 15:27:09 -0300

    Update run.py

--- run.py
+++ run.py
@@ -7,6 +7,13 @@ def home():
 @app.route("/status")
 def status():
     return {"status": "online", "nome": "AnaSync", "versão": "1.0"}
+@app.route("/ana")
+def ana():
+    return {
+        "nome": "Ana Cortez",
+        "versão": "1.0",
+        "função": "Gerar vídeos, postar em redes e vender como afiliada"
+    }
 
 if __name__ == "__main__":
     app.run()
