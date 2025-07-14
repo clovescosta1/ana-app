58c67613de417d0bcb9d5a55bbee9bbd1744837d
clovescosta1 <clovescosta2009@hotmail.com>
2025-07-14 15:24:29 -0300

    Update run.py

--- run.py
+++ run.py
@@ -4,6 +4,9 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return "AnaSync rodando!"
+@app.route("/status")
+def status():
+    return {"status": "online", "nome": "AnaSync", "versão": "1.0"}
 
 if __name__ == "__main__":
     app.run()
