994c1c94eb4bd4127b8ea92bfb70fc12e96670b3
clovescosta1 <clovescosta2009@hotmail.com>
2025-07-14 17:45:35 -0300

    Update run.py

--- run.py
+++ run.py
@@ -1,18 +1,25 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "AnaSync rodando!"
-@app.route("/status")
-def status():
-    return {"status": "online", "nome": "AnaSync", "versão": "1.0"}
+
 @app.route("/ana")
 def ana():
     return {
         "nome": "Ana Cortez",
         "versão": "1.0",
         "função": "Gerar vídeos, postar em redes e vender como afiliada"
+    }
+
+@app.route("/status")
+def status():
+    return {
+        "status": "online",
+        "servidor": "Render",
+        "app": "AnaSync"
     }
 
 if __name__ == "__main__":
